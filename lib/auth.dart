1ae8db10c76f25d714065c23a2d53f38e1b05e96
Omar Sharaki <omarsharaki@gmail.com>
2020-11-10 20:48:03 +0100

    :bug: Fix function not returning Future

--- lib/auth.dart
+++ lib/auth.dart
@@ -2,15 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 Future<UserCredential> anonymousSignIn() {
   print('Trying authentication 🤞🤞🤞🤞🤞🤞🤞🤞🤞🤞');
-  try {
-    FirebaseAuth.instance.signInAnonymously().then(
-      (UserCredential userCredential) {
-        userCredential = userCredential;
-        
-        return userCredential;
-      },
-    );
-  } on Exception catch (e) {
-    print('An authentication error occured!!!!!!!!!!!!!!');
-  }
+  Future<UserCredential> userCredential =
+      FirebaseAuth.instance.signInAnonymously().then((value) => value);
+  return userCredential;
 }
