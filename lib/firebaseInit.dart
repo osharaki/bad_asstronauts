4db39393cf14f3376a36814b0fb1a19b5f1335fb
Omar Sharaki <omarsharaki@gmail.com>
2020-11-11 13:30:59 +0100

    :poop: Added convenience code to quickly toggle between cloud and emulator instances

--- lib/firebaseInit.dart
+++ lib/firebaseInit.dart
@@ -29,8 +29,10 @@ class FirebaseInit extends StatelessWidget {
         // Once complete, show your application
         if (snapshot.connectionState == ConnectionState.done) {
           FirebaseFunctions.instance.useFunctionsEmulator(origin: 'http://localhost:5001');
+          // FirebaseFunctions.instance;
           FirebaseFirestore.instance.settings =
               Settings(host: '10.0.2.2:8080', sslEnabled: false, persistenceEnabled: false);
+          // FirebaseFirestore.instance;
           var boxGame = BoxGame();
           var tapper = TapGestureRecognizer();
           tapper.onTapDown = boxGame.onTapDown;
