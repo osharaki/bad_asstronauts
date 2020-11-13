06d4ce4378b29c3957b45c99b014b4f7946111d0
Omar Sharaki <omarsharaki@gmail.com>
2020-11-13 01:45:27 +0100

    :bug: Fix various logic and promise handling errors

--- cloud/functions/src/index.ts
+++ cloud/functions/src/index.ts
@@ -23,26 +23,33 @@ exports.addMessage = functions.https.onRequest(async (req, res) => {
 });
 
 exports.startGame = functions.https.onCall(async (data) => {
-    await start(data['sessionId']);
+    return start(data['sessionId']);
 })
 
 exports.endGame = functions.https.onCall(async (data) => {
-    await end(data['sessionId'], data['culprit']);
+    return end(data['sessionId'], data['culprit']);
 })
 
 const start = async (sessionId: string) => {
     let sec = 30;
-    setInterval(async () => {
-        sec--;
-        await admin.firestore().collection('sessions').doc(sessionId).update({ time: sec });
+    admin.firestore().collection('sessions').doc(sessionId).update({ time: sec });
+    let endTimer = setInterval(async () => {
+        admin.firestore().collection('sessions').doc(sessionId).update({ time: sec });
         if (sec === 0) {
-            await end(sessionId, null!);
+            clearInterval(endTimer);
+            end(sessionId, null!);
         }
+        sec--;
     }, 1000);
     const posX = Math.random() * 100;
     const posY = Math.random() * 100;
-    await admin.firestore().collection('sessions').doc(sessionId).update({ started: true, 'boxPosition': { posX: Math.floor(posX), posY: Math.floor(posY) } });
-    console.log('Started game successfully');
+    return admin.firestore().collection('sessions').doc(sessionId).update({ started: true, 'boxPosition': { posX: Math.floor(posX), posY: Math.floor(posY) } }).then((res) => {
+        if (res)
+            console.log('Started game successfully');
+        else
+            console.log('Something went wrong while starting game!');
+        return res;
+    });
 }
 
 const end = async (sessionId: string, culprit: string) => {
@@ -52,52 +59,66 @@ const end = async (sessionId: string, culprit: string) => {
     const p1 = players.docs[0];
     const p2 = players.docs[1];
     p1.data
-    let winnerId;
+    let winnerId: string;
     if (!culprit) {
         // time ran out
         if (p1.data()['score'] > p2.data()['score'])
-            winnerId = p1
+            winnerId = p1.id
+        else if (p1.data()['score'] > p2.data()['score'])
+            winnerId = p2.id
         else
-            winnerId = p2
+            winnerId = 'tie';
     }
     else if (culprit === p1.id)
         winnerId = p2.id;
     else
         winnerId = p1.id;
 
-    await admin.firestore().collection('sessions').doc(sessionId).update({ time: 30, started: false, boxPosition: { posX: 50, posY: 50 }, winner: winnerId });
-    console.log('Ended game successfully');
+    return admin.firestore().collection('sessions').doc(sessionId).update({ time: 30, started: false, boxPosition: { posX: 50, posY: 50 }, winner: winnerId }).then((res) => {
+        if (res)
+            console.log('Ended game successfully');
+        else
+            console.log('Something went wrong while ending game');
+        return res
+    });
 }
 
-
-
 exports.updateBoxPosition = functions.https.onCall(async (data) => {
-    // const screenHeight = data.screenHeight;
-    // const screenWidth = data.screenWidth;
-
     const posX = Math.random() * 100;
     const posY = Math.random() * 100;
     console.log(data['sessionId']);
-    await admin.firestore().collection('sessions').doc(data['sessionId']).update({ 'boxPosition': { posX: Math.floor(posX), posY: Math.floor(posY) } });
-    console.log('Position updated successfully');
+    return admin.firestore().collection('sessions').doc(data['sessionId']).update({ 'boxPosition': { posX: Math.floor(posX), posY: Math.floor(posY) } }).then((res) => {
+        if (res)
+            console.log('Position updated successfully');
+        else
+            console.log('Something went wrong while updating position');
+        return res
+    });
 });
 
 exports.initializeSession = functions.https.onCall(async (data) => {
-    await admin.firestore().collection('sessions').doc(data['sessionId']).set({ started: false, boxPosition: { posX: 50, posY: 50 }, time: 30, ready: false, startCountdown: 5 });
-    console.log('Initialized session successfully');
+    return admin.firestore().collection('sessions').doc(data['sessionId']).set({ started: false, boxPosition: { posX: 50, posY: 50 }, time: 30, ready: false, startCountdown: 5 }).then((res) => {
+        if (res)
+            console.log('Initialized session successfully');
+        else
+            console.log('Something went wrong while initializing session');
+        return res;
+    });
+
 })
 
 exports.incrementScore = functions.https.onCall(async (data) => {
-    await admin.firestore().collection('sessions').doc(data['sessionId']).collection('players').doc(data['playerId']).get().then(async (doc) => {
-        if (doc.exists) {
-            const player = await admin.firestore().collection('sessions').doc(data['sessionId']).collection('players').doc(data['playerId']);
-            if (doc.data()) {
-                await player.update({ score: doc.data()!['score'] + 1 });
-            }
-            console.log('Incremented score successfully');
-        }
+    return admin.firestore().collection('sessions').doc(data['sessionId']).collection('players').doc(data['playerId']).get().then(async (doc) => {
+        // The ! is an assurance by us that doc.data() is not null
+        doc.ref.update({ score: doc.data()!['score'] + 1 }).then((res) => {
+            if (res)
+                console.log('Incremented score successfully');
+            else
+                console.log('Something went wrong while incrementing score');
+            return res;
+        });
     });
-})
+});
 
 exports.getUser = functions.https.onRequest(async (req, res) => {
     admin.auth().getUser(req.query.uid as string).then((userRecord) => {
@@ -105,53 +126,51 @@ exports.getUser = functions.https.onRequest(async (req, res) => {
     }).catch(() => console.log('Error'));
 });
 
-exports.lock
-exports.onPlayerJoin = functions.firestore.document('sessions/{sessionId}/{players}/{playerId}').onCreate((snapshot, context) => {
+exports.onPlayerJoin = functions.firestore.document('sessions/{sessionId}/{players}/{playerId}').onCreate(async (snapshot, context) => {
     const playerId = context.params.playerId;
     const sessionId = context.params.sessionId;
     console.log(`Player ${playerId} joined session ${sessionId}`);
-    console.log(snapshot.data());
     const player = snapshot.ref;
-    const players = player.parent;
     console.log('Checking session status...');
-    return players.listDocuments().then(async (resolve) => {
-        if (resolve.length === 2) {
-            console.log('Session ready. Starting count down...');
-            await admin.firestore().collection('sessions').doc(sessionId).update({ ready: true });
-            let sec = 5;
-            setInterval(async () => {
-                sec--;
-                await admin.firestore().collection('sessions').doc(sessionId).update({ startCountdown: sec });
-                if (sec === 0) {
-                    // start game
-                    await start(sessionId);
-                }
-            }, 1000);
-        }
-        else { console.log('Session not ready'); console.log(resolve); }
-        return resolve;
-    }).catch((reject) => reject);
+    const players = await player.parent.listDocuments();
+    if (players.length === 2) {
+        console.log('Session ready. Starting count down...');
+        let sec = 5;
+        admin.firestore().collection('sessions').doc(sessionId).update({ startCountdown: sec });
+        let startTimer = setInterval(() => {
+            admin.firestore().collection('sessions').doc(sessionId).update({ startCountdown: sec });
+            if (sec === 0) {
+                clearInterval(startTimer)
+                // start game
+                start(sessionId);
+            }
+            sec--
+        }, 1000);
+        admin.firestore().collection('sessions').doc(sessionId).update({ ready: true });
+    }
+    else {
+        console.log('Session not ready. Awaiting second player.');
+    }
 });
 
 exports.onPlayerDelete = functions.firestore.document('sessions/{sessionId}/{players}/{playerId}').onDelete((snapshot, context) => {
     const playerId = context.params.playerId;
     const sessionId = context.params.sessionId;
     console.log(`Player ${playerId} left session ${sessionId}`);
-    console.log(snapshot.data());
     const player = snapshot.ref;
     const players = player.parent;
     console.log('Checking session status...');
-    return players.listDocuments().then(async (resolve) => {
-        if (resolve.length === 0) {
+    return players.listDocuments().then(async (res) => {
+        if (res.length === 0) {
             console.log('Session empty. Deleting...');
-            await players.parent?.delete(); // delete empty session
+            return players.parent?.delete(); // delete empty session
         }
         else {
             console.log('Session not empty');
-            console.log(resolve);
-            await admin.firestore().collection('sessions').doc(sessionId).update({ ready: false, startCountdown: 5 });
+            return admin.firestore().collection('sessions').doc(sessionId).update({ ready: false, startCountdown: 5 });
         }
-        return resolve;
+    }).then((res) => {
+        return res;
     }).catch((reject) => reject);
 });
 
