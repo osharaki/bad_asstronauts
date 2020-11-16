8af100aa11d83d0f634a66c170364e5ed686030f
Omar Sharaki <omarsharaki@gmail.com>
2020-11-16 22:38:33 +0100

    :recycle: Adapt to mobile app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,26 @@
 import express = require('express');
 const path = require('path');
 import http = require('http');
-import { Socket } from "socket.io";
+import { Socket } from 'socket.io';
 
-const socketio = require('socket.io');
 
 // Create a new express application instance
-const app: express.Application = express();
+const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const socketio = require('socket.io')(server);
 
+app.get('/', (req, res) => {
+  res.send("Node Server is running. Yay!!")
+});
 
-app.use(express.static('C:\\Users\\osharaki\\programming_misc_local\\Flutter\\game-off-2020\\server\\public'));
 server.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
-io.on("connection", (socket: Socket) => {
+socketio.on("connection", (socket: Socket) => {
   console.log(`connect ${socket.id}`);
-
+  console.log(`connect ${socket.handshake.url}`);
+  
   socket.on("disconnect", () => {
     console.log(`disconnect ${socket.id}`);
   });
