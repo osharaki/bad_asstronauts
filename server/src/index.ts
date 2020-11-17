3cadaf0eeeb9bfe0a7c1bafd83090009fd0b13f3
Omar Sharaki <omarsharaki@gmail.com>
2020-11-17 14:00:35 +0100

    :fire: :sparkles: Comment out uuid code. Broadcast data on message received

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,18 +3,26 @@ import WebSocket from 'ws';
 import { v4 as uuidv4 } from 'uuid';
 
 const wss = new WebSocket.Server({ port: 3000 });
-const clients = []
+// const clients = [];
 wss.addListener("listening", () => {
   console.log('WebSocket server running...');
 });
 
-wss.on('connection', (ws) => {
+wss.on('connection', ws => {
   console.log('Connection received!');
-  const uuid = uuidv4();
-  clients.push({ uuid: ws });
-  console.log(`Client ${uuid} added to list of clients!`);
-  ws.send(uuid);
-  ws.on('message', (msg) => {
-    console.log(`Client  ${uuid} said: ${msg}`);
+  // const uuid = uuidv4();
+  // clients.push({ uuid: ws });
+  // console.log(`Client ${uuid} added to list of clients!`);
+  // ws.send(uuid);
+  ws.on('message', (msg: string) => {
+    // console.log(`Client  ${uuid} said: ${msg}`);
+    console.log(`Client said: ${msg}`);
+    wss.clients.forEach(client => {
+      if (client != ws)
+        client.send(msg);
+    });
+  });
+  ws.on('close', reason => {
+    console.log('Client disconneted!');
   });
 });
