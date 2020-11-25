5a139e5e2f9b7d7239d1bcbbc0ce71bc9f648b81
Omar Sharaki <omarsharaki@gmail.com>
2020-11-25 12:07:10 +0100

    :construction: Convert spaceship to SpriteBodyComponent and add initial movement physics

--- lib/spaceship.dart
+++ lib/spaceship.dart
@@ -1,27 +1,58 @@
-import 'package:flame/components/component.dart';
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:flame/components/joystick/joystick_component.dart';
 import 'package:flame/components/joystick/joystick_events.dart';
-import 'package:flame/components/sprite_component.dart';
 import 'package:flame/extensions/vector2.dart';
 import 'package:flame/sprite.dart';
+import 'package:flame_forge2d/sprite_body_component.dart';
+import 'package:forge2d/forge2d.dart';
 
-class Spaceship extends SpriteComponent implements JoystickListener {
-  Spaceship(Sprite sprite, Vector2 size) : super.fromSprite(size, sprite);
+class Spaceship extends SpriteBodyComponent implements JoystickListener {
+  final Vector2 position;
+  final double speed = 159;
+  double currentSpeed = 0;
+  double radAngle = 0;
+  bool _move = false;
+
+  Spaceship(this.position, image, Vector2 size) : super(Sprite(image), size);
 
   @override
   void joystickAction(JoystickActionEvent event) {
-    if (event.event == ActionEvent.DOWN) {
-      if (event.id == 1) {
-        print('event.id == 1');
-      }
-      if (event.id == 2) {
-        print('event.id == 2');
-      }
-    }
+    // TODO: implement joystickChangeDirectional
   }
 
   @override
   void joystickChangeDirectional(JoystickDirectionalEvent event) {
-    // TODO: implement joystickChangeDirectional
+    _move = event.directional != JoystickMoveDirectional.IDLE;
+    if (_move) {
+      radAngle = event.radAngle;
+      currentSpeed = speed * event.intensity;
+    }
+  }
+
+  void moveFromAngle(double dtUpdate) {
+    final double nextX = (currentSpeed * dtUpdate) * cos(radAngle);
+    final double nextY = (currentSpeed * dtUpdate) * sin(radAngle);
+
+    // TODO figure out why this inversion is even necessary
+    body.applyLinearImpulse(Vector2(nextX, -nextY).scaled(20), body.worldCenter, true);
+  }
+
+  @override
+  Body createBody() {
+    // TODO body needs to come to a stop more quickly
+    final PolygonShape shape = PolygonShape();
+    shape.setAsBoxXY(size.x / 2, size.y / 2);
+
+    final fixtureDef = FixtureDef()..shape = shape;
+
+    final bodyDef = BodyDef()
+      // To be able to determine object in collision
+      ..setUserData(this)
+      ..position = viewport.getScreenToWorld(position)
+      ..type = BodyType.DYNAMIC;
+
+    return world.createBody(bodyDef)..createFixture(fixtureDef);
   }
 }
