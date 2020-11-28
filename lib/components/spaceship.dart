2c226bd4a99ed9bbd5ff7f07a54f46a25a99ea03
Omar Sharaki <omarsharaki@gmail.com>
2020-11-28 12:53:32 +0100

    :lipstick: Display spaceship resources as floating text

--- lib/components/spaceship.dart
+++ lib/components/spaceship.dart
@@ -1,10 +1,12 @@
 import 'dart:math';
 import 'dart:ui';
 
+import 'package:flame/anchor.dart';
 import 'package:flame/components/joystick/joystick_component.dart';
 import 'package:flame/components/joystick/joystick_events.dart';
 import 'package:flame/extensions/vector2.dart';
 import 'package:flame/sprite.dart';
+import 'package:flame/text_config.dart';
 import 'package:flame_forge2d/body_component.dart';
 import 'package:flutter/material.dart' hide Image;
 import 'package:forge2d/forge2d.dart';
@@ -15,7 +17,7 @@ class Spaceship extends BodyComponent implements JoystickListener {
   final Vector2 size;
   final Vector2 position;
   final double speed = 159;
-  final double capacity= 100;
+  final double capacity = 100;
   final String id;
 
   double resources = 100;
@@ -39,6 +41,7 @@ class Spaceship extends BodyComponent implements JoystickListener {
   @override
   void render(Canvas canvas) {
     super.render(canvas);
+
     canvas.save();
     Vector2 posRect = viewport.getWorldToScreen(body.worldCenter);
     Rect rect =
@@ -48,6 +51,18 @@ class Spaceship extends BodyComponent implements JoystickListener {
     canvas.translate(-posRect.x, -posRect.y);
     spaceship.renderRect(canvas, rect);
     canvas.restore();
+
+    TextConfig(
+      fontSize: 10.0,
+      fontFamily: 'Awesome Font',
+      textAlign: TextAlign.center,
+    ).render(
+      canvas,
+      resources.toStringAsFixed(2),
+      game.viewport
+          .getWorldToScreen(Vector2(body.worldCenter.x, body.worldCenter.y + size.y / 2 - 40)),
+      anchor: Anchor.center,
+    );
   }
 
   @override
